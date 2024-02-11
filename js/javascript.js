25b49b1c54c7cc45a57b3a8ca7babc48db8215b9
dalia.m.gonzalez <dalia.m.gzz@gmail.com>
2024-02-11 10:12:03 -0600

    Commit inicial

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+alert("Enlace externo al Laboratorio Cultural Ciudadano Nuevo León."+
+"\n"+"\nAquí, a partir de un código QR se entrega al usuario información de los préstamos de piezas e instalaciones que administra el LabNL."+"\n"+
+"\nDespués de presionar el botón de OK, Para entrar a la página oficial de LabNL, presiona el icono superior derecho de la página.")
+
+alert("Es mas fácil hacer lo correcto!!" + "\nAtte.: Los Martes")
+
+/*alert("Segunda alerta, por si se ocupa..." + "\n"+"\nDisfruta la vida, lo único que tienes es: ¡¡hoy!!" + "\nAtte.: Los Martes.")*/
